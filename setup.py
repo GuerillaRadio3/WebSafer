dc24e522996f36583dc972202e5fdc3bdafd9fbc
IsaiahStanke <83856624+IsaiahStanke@users.noreply.github.com>
2022-03-24 01:01:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     license='GNU General Public License v3.0',
     author='Isaiah Stanke',
     author_email='',
-    description='A tool that blocks websites on single computer\'s to make the Web safer'
+    description='A tool that blocks websites on single computer\'s to make the Web safer',
+    py_modules=['colorama', 'icmplib', 'lib2to3.pytree', 'shutil', 'os', 'time', 'os.path', 'pathlib']
 )
