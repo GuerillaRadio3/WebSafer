e36df1a648c58ac19768d5e9309d5ba8d9f939cf
IsaiahStanke <83856624+IsaiahStanke@users.noreply.github.com>
2022-03-24 01:08:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setuptools.setup(
     license='GNU General Public License v3.0',
     author='Isaiah Stanke',
     author_email='',
-    description='A tool that blocks websites on single computer\'s to make the Web safer'
+    description='A tool that blocks websites on single computer\'s to make the Web safer',
+    install_requires='icmplib'
 )
