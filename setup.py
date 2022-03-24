6cd9910d5db24f04fc89590d1045433a247955ba
IsaiahStanke <83856624+IsaiahStanke@users.noreply.github.com>
2022-03-24 00:36:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WebSafer',
-    version='0.10',
+    version='0.1.0',
     packages=find_packages(include=['WebSafer', 'WebSafer.*']),
     url='https://github.com/IsaiahStanke/WebSafer',
     license='GNU General Public License v3.0',
