f228d8342e4b431c90bd7d5fedf634592f954446
IsaiahStanke <83856624+IsaiahStanke@users.noreply.github.com>
2022-03-24 01:03:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='WebSafer',
     version='0.1.0',
-    packages=find_packages(include=['WebSafer', 'WebSafer.*']),
+    packages=setuptools.find_packages(),
     url='https://github.com/IsaiahStanke/WebSafer',
     license='GNU General Public License v3.0',
     author='Isaiah Stanke',
     author_email='',
-    description='A tool that blocks websites on single computer\'s to make the Web safer',
-    py_modules=['colorama', 'icmplib', 'lib2to3.pytree', 'shutil', 'os', 'time', 'os.path', 'pathlib']
+    description='A tool that blocks websites on single computer\'s to make the Web safer'
 )
